3c088eb3bb15efe04a64cfa3c8366b74c410e63a
Bengt J <2410396+bengtj@users.noreply.github.com>
2024-06-08 15:10:38 +0200

    Upgraded home assistant

--- custom_components/nordpool/__init__.py
+++ custom_components/nordpool/__init__.py
@@ -1,26 +1,30 @@
 import logging
 from collections import defaultdict
-from datetime import datetime, timedelta
-from functools import partial
+from datetime import timedelta
 from random import randint
 
+import backoff
 import voluptuous as vol
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Config, HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.dispatcher import async_dispatcher_send
-from homeassistant.helpers.event import async_call_later, async_track_time_change
+from homeassistant.helpers.event import async_track_time_change
 from homeassistant.util import dt as dt_utils
 from pytz import timezone
 
-from .aio_price import AioPrices
+from .aio_price import AioPrices, InvalidValueException
 from .events import async_track_time_change_in_tz
 
 DOMAIN = "nordpool"
 _LOGGER = logging.getLogger(__name__)
 RANDOM_MINUTE = randint(10, 30)
 RANDOM_SECOND = randint(0, 59)
-EVENT_NEW_DATA = "nordpool_update"
+EVENT_NEW_HOUR = "nordpool_update_hour"
+EVENT_NEW_DAY = "nordpool_update_day"
+EVENT_NEW_PRICE = "nordpool_update_new_price"
+SENTINEL = object()
+
 _CURRENCY_LIST = ["DKK", "EUR", "NOK", "SEK"]
 
 
@@ -28,7 +32,7 @@ CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
 
 NAME = DOMAIN
-VERSION = "0.0.7"
+VERSION = "0.0.14"
 ISSUEURL = "https://github.com/custom-components/nordpool/issues"
 
 STARTUP = f"""
@@ -43,108 +47,125 @@ If you have any issues with this you need to open an issue here:
 
 
 class NordpoolData:
+    """Holds the data"""
+
     def __init__(self, hass: HomeAssistant):
         self._hass = hass
         self._last_tick = None
         self._data = defaultdict(dict)
-        self._tomorrow_valid = False
         self.currency = []
         self.listeners = []
+        self.areas = []
 
-    async def _update(self, type_="today", dt=None):
-        _LOGGER.debug("calling _update %s %s", type_, dt)
+    async def _update(self, type_="today", dt=None, areas=None):
+        _LOGGER.debug("calling _update %s %s %s", type_, dt, areas)
         hass = self._hass
         client = async_get_clientsession(hass)
 
         if dt is None:
             dt = dt_utils.now()
 
+        if areas is not None:
+            self.areas += [area for area in areas if area not in self.areas]
         # We dont really need today and morrow
         # when the region is in another timezone
         # as we request data for 3 days anyway.
         # Keeping this for now, but this should be changed.
         for currency in self.currency:
             spot = AioPrices(currency, client)
-            data = await spot.hourly(end_date=dt)
+            data = await spot.hourly(end_date=dt, areas=self.areas if len(self.areas) > 0 else None)
             if data:
                 self._data[currency][type_] = data["areas"]
-            else:
-                _LOGGER.info("Some crap happend, retrying request later.")
-                async_call_later(hass, 20, partial(self._update, type_=type_, dt=dt))
 
-    async def update_today(self, n: datetime):
-        _LOGGER.debug("Updating tomorrows prices.")
-        await self._update("today")
+    async def update_today(self, areas=None):
+        """Update today's prices"""
+        _LOGGER.debug("Updating today's prices.")
+        if areas is not None:
+            self.areas += [area for area in areas if area not in self.areas]
+        await self._update("today", areas=self.areas if len(self.areas) > 0 else None)
 
-    async def update_tomorrow(self, n: datetime):
+    async def update_tomorrow(self, areas=None):
+        """Update tomorrows prices."""
         _LOGGER.debug("Updating tomorrows prices.")
-        await self._update(type_="tomorrow", dt=dt_utils.now() + timedelta(hours=24))
-        self._tomorrow_valid = True
+        if areas is not None:
+            self.areas += [area for area in areas if area not in self.areas]
+        await self._update(type_="tomorrow", dt=dt_utils.now() + timedelta(hours=24), areas=self.areas if len(self.areas) > 0 else None)
 
     async def _someday(self, area: str, currency: str, day: str):
-        """Returns todays or tomorrows prices in a area in the currency"""
+        """Returns today's or tomorrow's prices in an area in the currency"""
         if currency not in _CURRENCY_LIST:
             raise ValueError(
-                "%s is a invalid currency possible values are %s"
+                "%s is an invalid currency, possible values are %s"
                 % (currency, ", ".join(_CURRENCY_LIST))
             )
 
+        if area not in self.areas:
+            self.areas.append(area);
         # This is needed as the currency is
         # set in the sensor.
         if currency not in self.currency:
             self.currency.append(currency)
-            await self.update_today(None)
-            await self.update_tomorrow(None)
+            try:
+                await self.update_today(areas=self.areas)
+            except InvalidValueException:
+                _LOGGER.debug("No data available for today, retrying later")
+            try:
+                await self.update_tomorrow(areas=self.areas)
+            except InvalidValueException:
+                _LOGGER.debug("No data available for tomorrow, retrying later")
+
+            # Send a new data request after new data is updated for this first run
+            # This way if the user has multiple sensors they will all update
+            async_dispatcher_send(self._hass, EVENT_NEW_HOUR)
 
         return self._data.get(currency, {}).get(day, {}).get(area)
 
-    def tomorrow_valid(self) -> bool:
-        return self._tomorrow_valid
-
     async def today(self, area: str, currency: str) -> dict:
-        """Returns todays prices in a area in the requested currency"""
-        res = await self._someday(area, currency, "today")
-        return res
+        """Returns today's prices in an area in the requested currency"""
+        return await self._someday(area, currency, "today")
+        
 
     async def tomorrow(self, area: str, currency: str):
-        """Returns tomorrows prices in a area in the requested currency"""
-        res = await self._someday(area, currency, "tomorrow")
-        return res
-
+        """Returns tomorrow's prices in an area in the requested currency"""
+        return await self._someday(area, currency, "tomorrow")
+        
 
 async def _dry_setup(hass: HomeAssistant, config: Config) -> bool:
     """Set up using yaml config file."""
-
     if DOMAIN not in hass.data:
         api = NordpoolData(hass)
         hass.data[DOMAIN] = api
+        _LOGGER.debug("Added %s to hass.data", DOMAIN)
 
-        async def new_day_cb(n):
+        async def new_day_cb(_):
             """Cb to handle some house keeping when it a new day."""
             _LOGGER.debug("Called new_day_cb callback")
-            api._tomorrow_valid = False
 
             for curr in api.currency:
-                if not len(api._data[curr]["tomorrow"]):
-                    api._data[curr]["today"] = await api.update_today(None)
+                if not api._data.get(curr, {}).get("tomorrow"):
+                    api._data[curr]["today"] = await api.update_today()
                 else:
                     api._data[curr]["today"] = api._data[curr]["tomorrow"]
                 api._data[curr]["tomorrow"] = {}
 
-            async_dispatcher_send(hass, EVENT_NEW_DATA)
+            async_dispatcher_send(hass, EVENT_NEW_DAY)
 
-        async def new_hr(n):
+        async def new_hr(_):
             """Callback to tell the sensors to update on a new hour."""
             _LOGGER.debug("Called new_hr callback")
-            async_dispatcher_send(hass, EVENT_NEW_DATA)
+            async_dispatcher_send(hass, EVENT_NEW_HOUR)
 
-        async def new_data_cb(n):
+        @backoff.on_exception(
+            backoff.constant,
+            (InvalidValueException),
+            logger=_LOGGER, interval=600, max_time=7200, jitter=None)
+        async def new_data_cb(_):
             """Callback to fetch new data for tomorrows prices at 1300ish CET
             and notify any sensors, about the new data
             """
-            _LOGGER.debug("Called new_data_cb")
-            await api.update_tomorrow(n)
-            async_dispatcher_send(hass, EVENT_NEW_DATA)
+            # _LOGGER.debug("Called new_data_cb")
+            await api.update_tomorrow()
+            async_dispatcher_send(hass, EVENT_NEW_PRICE)
 
         # Handles futures updates
         cb_update_tomorrow = async_track_time_change_in_tz(
@@ -181,7 +202,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
 
-    # entry.add_update_listener(async_reload_entry)
+    entry.add_update_listener(async_reload_entry)
     return res
 
 
@@ -190,8 +211,12 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
 
     if unload_ok:
-        for unsub in hass.data[DOMAIN].listeners:
-            unsub()
+        # This is an issue if you have multiple sensors as everything related to DOMAIN
+        # is removed, regardless if you have multiple sensors or not. Doesn't seem to
+        # create a big issue for now #TODO
+        if DOMAIN in hass.data:
+            for unsub in hass.data[DOMAIN].listeners:
+                unsub()
         hass.data.pop(DOMAIN)
 
         return True
